a3349fd90e91729b643051c8216b8532cfa8e360
Rita <margvlahou@gmail.com>
2021-08-29 16:35:45 +0100

    focus nav

--- components/Navbar/NavLinkMobile.jsx
+++ components/Navbar/NavLinkMobile.jsx
@@ -3,7 +3,7 @@ function NavLinkMobile({ href, children, role }) {
   return (
     <div>
       <Link href={href}>
-        <a className="text-xs font-semibold text-white hover:text-gray-900 hover:bg-gray-50 focus:outline-none  focus:bg-gray-50 transition duration-150 ease-in-out text-center tracking-2 focus:text-p1 focus:shadow-ouline">
+        <a className="text-xs font-semibold text-white hover:text-gray-900 transition duration-150 ease-in-out text-center tracking-2 focus:text-p1">
           {children}
         </a>
       </Link>
